bc69c9a06cfc7746192c3132f88a52adf2d73288
TGMick <131347702+TGMick@users.noreply.github.com>
2023-06-09 10:36:58 +0100

    Add files via upload

---
+++ LittleLemonResturantApp/LittleLemonResturantApp/LittleLemonLogo.swift
@@ -0,0 +1,20 @@
+//
+//  LittleLemonLogo.swift
+//  LittleLemonRestaurant
+//
+//  Created by Michael Abrams on 6/7/23.
+//
+
+import SwiftUI
+
+struct LittleLemonLogo: View {
+    var body: some View {
+        Image("littleLemon")
+    }
+}
+
+struct LittleLemonLogo_Previews: PreviewProvider {
+    static var previews: some View {
+        LittleLemonLogo()
+    }
+}
